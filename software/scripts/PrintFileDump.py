3bf22a1e3563216cb00b2541e770173b2977e141
Larry Ruckman <ruckman@slac.stanford.edu>
2018-11-26 19:54:45 -0800

    script update

--- software/scripts/PrintFileDump.py
+++ software/scripts/PrintFileDump.py
@@ -11,12 +11,37 @@
 
 import pyrogue as pr
 import rogue.utilities.fileio
+import numpy as np
 
 import common as feb
 
 import argparse
 import sys
 import os
+
+#################################################################
+
+class MyEventReader(rogue.interfaces.stream.Slave):
+
+    def __init__(self):
+        rogue.interfaces.stream.Slave.__init__(self)
+
+    def _acceptFrame(self,frame):
+        # Get the payload data
+        p = bytearray(frame.getPayload())
+        # Return the buffer index
+        frame.read(p,0)
+        # Check for a 32-bit word
+        if len(p) == 4:
+            # Combine the byte array into single 32-bit word
+            hitWrd = np.frombuffer(p, dtype='uint32', count=1)
+            # Parse the 32-bit word
+            seqCnt = (hitWrd[0] >> 19) & 0x1FFF
+            tot    = (hitWrd[0] >>  9) & 0x3FF
+            toa    = (hitWrd[0] >>  1) & 0xFF
+            hit    = (hitWrd[0] >>  0) & 0x1
+            # Print the event
+            print( 'Event[seqCnt=0x%x]: tot = 0x%x, tot = 0x%x, hit=%d' % (seqCnt,tot,toa,hit) )
 
 #################################################################
 
@@ -40,7 +65,7 @@ args = parser.parse_args()
 dataReader = rogue.utilities.fileio.StreamReader()
 
 # Tap the streaming data interface (same interface that writes to file)
-dataStream = feb.ExampleEventReader()    
+dataStream = MyEventReader()    
 pr.streamTap(dataReader, dataStream) 
 
 # Open the file
