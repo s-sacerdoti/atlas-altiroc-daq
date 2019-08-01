d8768639e23a1c00420dc90f0b401c490d567f1c
cmilke <chrisdmilke@gmail.com>
2019-08-01 09:58:49 -0700

    changed config to old version...

--- software/scripts/thresholdScan.py
+++ software/scripts/thresholdScan.py
@@ -60,70 +60,147 @@ import matplotlib.pyplot as plt                                ##
 
 
 def set_fpga_for_custom_config(top):
+                                                               ##
+if Disable_CustomConfig == 0:                                  ## 
+                                                               ##
     top.Asic.Probe.en_probe_pa.set(0x0)                        ##
-
-    for i in range(25):
-        top.Fpga[0].Asic.SlowControl.disable_pa[i].set(0x1)
-        top.Fpga[0].Asic.SlowControl.ON_discri[i].set(0x0)
-        top.Fpga[0].Asic.SlowControl.EN_ck_SRAM[i].set(0x1)
-        top.Fpga[0].Asic.SlowControl.EN_trig_ext[i].set(0x0)
-        top.Fpga[0].Asic.SlowControl.ON_Ctest[i].set(0x0)
-
-        top.Fpga[0].Asic.SlowControl.cBit_f_TOA[i].set(0x0)
-        top.Fpga[0].Asic.SlowControl.cBit_s_TOA[i].set(0x0)
-        top.Fpga[0].Asic.SlowControl.cBit_f_TOT[i].set(0x0)
-        top.Fpga[0].Asic.SlowControl.cBit_s_TOT[i].set(0x0)
-        top.Fpga[0].Asic.SlowControl.cBit_c_TOT[i].set(0x0)
-
-    for i in range(16):
-        top.Fpga[0].Asic.SlowControl.EN_trig_ext[i].set(0x0)
-
-    top.Fpga[0].Asic.SlowControl.disable_pa[pixel_number].set(0x0)
-    top.Fpga[0].Asic.SlowControl.ON_discri[pixel_number].set(0x1)
-    top.Fpga[0].Asic.SlowControl.EN_hyst[pixel_number].set(0x1)
-    top.Fpga[0].Asic.SlowControl.EN_trig_ext[pixel_number].set(0x0)
-    top.Fpga[0].Asic.SlowControl.EN_ck_SRAM[pixel_number].set(0x1)
-    top.Fpga[0].Asic.SlowControl.ON_Ctest[pixel_number].set(0x1)
-    top.Fpga[0].Asic.SlowControl.bit_vth_cor[pixel_number].set(0x30)
-
-    top.Fpga[0].Asic.SlowControl.Write_opt.set(0x0)
-    top.Fpga[0].Asic.SlowControl.Precharge_opt.set(0x0)
-
-    top.Fpga[0].Asic.SlowControl.DLL_ALockR_en.set(0x1)
-    top.Fpga[0].Asic.SlowControl.CP_b.set(0x5) #5
-    top.Fpga[0].Asic.SlowControl.ext_Vcrtlf_en.set(0x0) #0
-    top.Fpga[0].Asic.SlowControl.ext_Vcrtls_en.set(0x1) #1
-    top.Fpga[0].Asic.SlowControl.ext_Vcrtlc_en.set(0x0) #0
-
-    top.Fpga[0].Asic.SlowControl.totf_satovfw.set(0x1)
-    top.Fpga[0].Asic.SlowControl.totc_satovfw.set(0x1)
-    top.Fpga[0].Asic.SlowControl.toa_satovfw.set(0x1)
-
-    top.Fpga[0].Asic.SlowControl.SatFVa.set(0x3)
-    top.Fpga[0].Asic.SlowControl.IntFVa.set(0x1)
-    top.Fpga[0].Asic.SlowControl.SatFTz.set(0x4)
-    top.Fpga[0].Asic.SlowControl.IntFTz.set(0x1)
+    #top.Asic.Probe.en_probe_dig.set(0x0)
+                                                               ##
+    for i in range(25):                                        ##
+        top.Asic.Probe.pix[i].probe_pa.set(0x0)                ## 
+        top.Asic.Probe.pix[i].probe_vthc.set(0x0)              ## 
+        top.Asic.Probe.pix[i].probe_dig_out_disc.set(0x0)      ##
+        top.Asic.Probe.pix[i].probe_toa.set(0x0)               ##
+        top.Asic.Probe.pix[i].probe_tot.set(0x0)               ##
+        top.Asic.Probe.pix[i].totf.set(0x0)                    ##
+        top.Asic.Probe.pix[i].tot_overflow.set(0x0)            ##
+        top.Asic.Probe.pix[i].toa_busy.set(0x0)                ##
+        top.Asic.Probe.pix[i].toa_ready.set(0x0)               ##
+        top.Asic.Probe.pix[i].tot_busy.set(0x0)                ##
+        top.Asic.Probe.pix[i].tot_ready.set(0x0)               ##
+        top.Asic.Probe.pix[i].en_read.set(0x0)                 ##
     
-    top.Fpga[0].Asic.SlowControl.cBitf.set(0x0) #0
-    top.Fpga[0].Asic.SlowControl.cBits.set(0xf) #f
-    top.Fpga[0].Asic.SlowControl.cBitc.set(0xf) #f
+    if not useProbe:
+        top.Asic.Probe.en_probe_dig.set(0x0)
+                                                           ##
+    if pixel_number in range(0, 5):                            ##
+        if useProbe:
+            top.Asic.Probe.en_probe_dig.set(0x1)                   ## 0x1
+        top.Asic.Probe.EN_dout.set(0x1)                        ##
+    if pixel_number in range(5, 10):                           ##
+        if useProbe:top.Asic.Probe.en_probe_dig.set(0x2)                   ## 0x2
+        top.Asic.Probe.EN_dout.set(0x2)                        ##
+    if pixel_number in range(10, 15):                          ##
+        if useProbe:top.Asic.Probe.en_probe_dig.set(0x4)                   ## 0x4
+        top.Asic.Probe.EN_dout.set(0x4)                        ##
+    if pixel_number in range(15, 20):                          ##
+        if useProbe:top.Asic.Probe.en_probe_dig.set(0x8)                   ## 0x8
+        top.Asic.Probe.EN_dout.set(0x8)                        ##
+    if pixel_number in range(20, 25):                          ##
+        if useProbe:top.Asic.Probe.en_probe_dig.set(0x10)                  ##
+        top.Asic.Probe.EN_dout.set(0x10)                       ##
+                                                           ##
+    top.Asic.Gpio.RSTB_READ.set(0x0)                           ##
+    time.sleep(0.1)                                            ##
+    top.Asic.Gpio.RSTB_READ.set(0x1)                           ##
+    
+    if useProbe:                                                           ##
+        top.Asic.Probe.pix[pixel_number].probe_pa.set(0x0)         ## 
+        top.Asic.Probe.pix[pixel_number].probe_vthc.set(0x0)       ## 
+        top.Asic.Probe.pix[pixel_number].probe_dig_out_disc.set(0x1)#
+        top.Asic.Probe.pix[pixel_number].probe_toa.set(0x0)        ##
+        top.Asic.Probe.pix[pixel_number].probe_tot.set(0x0)        ##
+        top.Asic.Probe.pix[pixel_number].totf.set(0x0)             ##
+        top.Asic.Probe.pix[pixel_number].tot_overflow.set(0x0)     ##
+        top.Asic.Probe.pix[pixel_number].toa_busy.set(0x0)         ##
+        top.Asic.Probe.pix[pixel_number].toa_ready.set(0x0)        ##
+        top.Asic.Probe.pix[pixel_number].tot_busy.set(0x0)         ##
+        top.Asic.Probe.pix[pixel_number].tot_ready.set(0x0)        ##
 
-    top.Fpga[0].Asic.SlowControl.cBit_f_TOA[pixel_number].set(0x0)  #0
-    top.Fpga[0].Asic.SlowControl.cBit_s_TOA[pixel_number].set(0x0)  #0
-    top.Fpga[0].Asic.SlowControl.cBit_f_TOT[pixel_number].set(0xf)  #f
-    top.Fpga[0].Asic.SlowControl.cBit_s_TOT[pixel_number].set(0x0)  #0
-    top.Fpga[0].Asic.SlowControl.cBit_c_TOT[pixel_number].set(0xf)  #f
-    top.Fpga[0].Asic.SlowControl.Rin_Vpa.set(0x1) #0
-    top.Fpga[0].Asic.SlowControl.cd[0].set(0x0) #6
-    top.Fpga[0].Asic.SlowControl.dac_biaspa.set(0x10) #10
-    top.Fpga[0].Asic.SlowControl.dac_pulser.set(0x7) #7
-    top.Fpga[0].Asic.SlowControl.DAC10bit.set(0x19f) #173 / 183
+    top.Asic.Probe.pix[pixel_number].en_read.set(0x1)          ##
+#################################################################
 
-    top.Fpga[0].Asic.Gpio.DlyCalPulseSet.set(0x0)   # Rising edge of EXT_TRIG or CMD_PULSE delay
-    top.Fpga[0].Asic.Gpio.DlyCalPulseReset.set(0xfff) # Falling edge of EXT_TRIG (independent of CMD_PULSE)
+#################################################################
+# Custom Configuration                                         ##
+                                                               ##
+if Disable_CustomConfig == 0:                                  ##
+                                                               ##
+    for i in range(25):                                        ##
+        top.Asic.SlowControl.disable_pa[i].set(0x1)            ##
+        top.Asic.SlowControl.ON_discri[i].set(0x0)             ##
+        top.Asic.SlowControl.EN_ck_SRAM[i].set(0x1)            ##
+        top.Asic.SlowControl.EN_trig_ext[i].set(0x0)           ##
+        top.Asic.SlowControl.ON_Ctest[i].set(0x0)              ##
+                                                               ##
+        top.Asic.SlowControl.cBit_f_TOA[i].set(0x0)            ##
+        top.Asic.SlowControl.cBit_s_TOA[i].set(0x0)            ##      
+        top.Asic.SlowControl.cBit_f_TOT[i].set(0x0)            ##
+        top.Asic.SlowControl.cBit_s_TOT[i].set(0x0)            ##
+        top.Asic.SlowControl.cBit_c_TOT[i].set(0x0)            ##
+                                                               ##
+    for i in range(16):                                        ##
+        top.Asic.SlowControl.EN_trig_ext[i].set(0x0)           ##
+                                                               ##
+    top.Asic.SlowControl.disable_pa[pixel_number].set(0x0)     ##
+    top.Asic.SlowControl.ON_discri[pixel_number].set(0x1)      ##
+    top.Asic.SlowControl.EN_hyst[pixel_number].set(0x1)        ##
+    top.Asic.SlowControl.EN_trig_ext[pixel_number].set(0x0)    ##
+    top.Asic.SlowControl.EN_ck_SRAM[pixel_number].set(0x1)     ##
+    top.Asic.SlowControl.ON_Ctest[pixel_number].set(0x1)       ##
+    ##top.Asic.SlowControl.bit_vth_cor[pixel_number].set(0x40)   ##
+                                                               ##
+    top.Asic.SlowControl.Write_opt.set(0x0)                    ##
+    top.Asic.SlowControl.Precharge_opt.set(0x0)                ##
+                                                               ##
+    top.Asic.SlowControl.DLL_ALockR_en.set(0x1)                ##
+    top.Asic.SlowControl.CP_b.set(0x5) #5                      ##
+    top.Asic.SlowControl.ext_Vcrtlf_en.set(0x0) #1             ##
+    top.Asic.SlowControl.ext_Vcrtls_en.set(0x0) #1             ##
+    top.Asic.SlowControl.ext_Vcrtlc_en.set(0x0) #1             ##
+                                                               ##
+    top.Asic.SlowControl.totf_satovfw.set(0x1)                 ##
+    top.Asic.SlowControl.totc_satovfw.set(0x1)                 ##
+    top.Asic.SlowControl.toa_satovfw.set(0x1)                  ##
+                                                               ##
+    top.Asic.SlowControl.SatFVa.set(0x3)                       ##
+    top.Asic.SlowControl.IntFVa.set(0x1)                       ##
+    top.Asic.SlowControl.SatFTz.set(0x4)                       ##
+    top.Asic.SlowControl.IntFTz.set(0x1)                       ##
+                                                               ##
+    top.Asic.SlowControl.cBitf.set(0x0) #0                     ##
+    top.Asic.SlowControl.cBits.set(0xf) #f                     ##
+    top.Asic.SlowControl.cBitc.set(0xf) #f                     ##
+                                                               ##
+    top.Asic.SlowControl.cBit_f_TOA[pixel_number].set(0x0) #f  ##
+    top.Asic.SlowControl.cBit_s_TOA[pixel_number].set(0x0) #0  ##
+                                                               ##
+    top.Asic.SlowControl.cBit_f_TOT[pixel_number].set(0xf) #f  ##
+    top.Asic.SlowControl.cBit_s_TOT[pixel_number].set(0x0) #0  ##
+    top.Asic.SlowControl.cBit_c_TOT[pixel_number].set(0xf) #0  ##
+                                                               ##
+    top.Asic.SlowControl.Rin_Vpa.set(0x0) #0                   ##
+    top.Asic.SlowControl.Cp_Vpa.set(0x0) #0                    ##
+    top.Asic.SlowControl.cd[0].set(0x7) #6                     ## DON'T FORGET to change cd [] 0, 1 or 2 for ch4, 9 or 14
+    top.Asic.SlowControl.cd[1].set(0x7) #6                     ## DON'T FORGET to change cd [] 0, 1 or 2 for ch4, 9 or 14
+    top.Asic.SlowControl.cd[2].set(0x7) #6                     ## DON'T FORGET to change cd [] 0, 1 or 2 for ch4, 9 or 14
+    top.Asic.SlowControl.dac_biaspa.set(30) #10 30= 0x1e 60=0x3c ##
+    top.Asic.SlowControl.dac_pulser.set(Qinj) # Modified Pulser Decimal code 3 = 2.66,6 = 5.24, 12 =10.32 fC, 24=20.08 fC  ##
+    top.Asic.SlowControl.DAC10bit.set(398)           ##
+    top.SysReg.DlyData.set(DelayValueTOA)
+                                                           ##
+    top.Asic.DoutDebug.DeserSampleEdge.set(0x0)                ##
+                                                               ##
+    top.Asic.PulseTrain.PulseCount.set(0x1)                    ## 0X1
+    top.Asic.PulseTrain.PulseWidth.set(0x8)                    ## 0X8
+    top.Asic.PulseTrain.PulsePeriod.set(0x4)                   ## 0X4
+    top.Asic.PulseTrain.PulseDelay.set(0x4)                    ## 0X4
+    top.Asic.PulseTrain.ReadDelay.set(0x9)                     ## 0X8
+    top.Asic.PulseTrain.ReadDuration.set(0x70) #10a0           ## 0X70
+    top.Asic.PulseTrain.ResetCounterMask.set(0x3)              ## 0X3
+    top.Asic.PulseTrain.ResetTdcMask.set(0x2)                  ## 0X2
+                                                               ##
+#################################################################
 
-    top.Fpga[0].Asic.Readout.StartPix.set(pixel_number)
-    top.Fpga[0].Asic.Readout.LastPix.set(pixel_number)
 
 #################################################################
 
