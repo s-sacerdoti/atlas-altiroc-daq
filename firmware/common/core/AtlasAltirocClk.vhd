9a93e75b69fd5fd51721ac9b192f50b87c3c13a9
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-07 14:10:50 -0800

    Not Synchronizing DC/DC to PLL and U_rst160MHz back to RstSync

--- firmware/common/core/AtlasAltirocClk.vhd
+++ firmware/common/core/AtlasAltirocClk.vhd
@@ -52,21 +52,21 @@ end AtlasAltirocClk;
 
 architecture mapping of AtlasAltirocClk is
 
-   type RegType is record
-      fastCnt     : natural range 0 to 5;
-      slowCnt     : natural range 0 to 23;
-      fastSyncClk : sl;
-      slowSyncClk : sl;
-   end record;
+   -- type RegType is record
+      -- fastCnt     : natural range 0 to 5;
+      -- slowCnt     : natural range 0 to 23;
+      -- fastSyncClk : sl;
+      -- slowSyncClk : sl;
+   -- end record;
 
-   constant REG_INIT_C : RegType := (
-      fastCnt     => 0,
-      slowCnt     => 0,
-      fastSyncClk => '0',
-      slowSyncClk => '0');
+   -- constant REG_INIT_C : RegType := (
+      -- fastCnt     => 0,
+      -- slowCnt     => 0,
+      -- fastSyncClk => '0',
+      -- slowSyncClk => '0');
 
-   signal r   : RegType := REG_INIT_C;
-   signal rin : RegType;
+   -- signal r   : RegType := REG_INIT_C;
+   -- signal rin : RegType;
 
    signal localRefClock : sl;
    signal localRefClk   : sl;
@@ -121,15 +121,15 @@ begin
          I => pllClkIn(0),
          O => clock160MHz);
 
-   U_rst160MHz : entity work.PwrUpRst
+   U_rst160MHz : entity work.RstSync
       generic map(
          TPD_G          => TPD_G,
          IN_POLARITY_G  => '0',
          OUT_POLARITY_G => '1')
       port map (
-         clk    => clock160MHz,
-         arst   => pllLolL,
-         rstOut => reset160MHz);
+         clk      => clock160MHz,
+         asyncRst => pllLolL,
+         syncRst  => reset160MHz);
 
    clk160MHz <= clock160MHz;
    rst160MHz <= reset160MHz;
@@ -153,59 +153,65 @@ begin
 
    strb40MHz <= strobe40MHz;
 
-   ------------------------------------------
-   -- Synchronizing the DC/DC to 40 MHz clock
-   ------------------------------------------
-   comb : process (r) is
-      variable v : RegType;
-   begin
-      -- Latch the current value
-      v := r;
+   -- ------------------------------------------
+   -- -- Synchronizing the DC/DC to 40 MHz clock
+   -- ------------------------------------------
+   -- comb : process (r) is
+      -- variable v : RegType;
+   -- begin
+      -- -- Latch the current value
+      -- v := r;
 
-      -- Check for last count
-      if r.fastCnt = 5 then
-         -- Reset the counter
-         v.fastCnt     := 0;
-         -- Toggle the flag
-         v.fastSyncClk := not (r.fastSyncClk);  -- 3.33 MHz = 40MHz/(2 x 6)
-      else
-         -- Increment the counter
-         v.fastCnt := r.fastCnt + 1;
-      end if;
+      -- -- Check for last count
+      -- if r.fastCnt = 5 then
+         -- -- Reset the counter
+         -- v.fastCnt     := 0;
+         -- -- Toggle the flag
+         -- v.fastSyncClk := not (r.fastSyncClk);  -- 3.33 MHz = 40MHz/(2 x 6)
+      -- else
+         -- -- Increment the counter
+         -- v.fastCnt := r.fastCnt + 1;
+      -- end if;
 
-      -- Check for last count
-      if r.slowCnt = 23 then
-         -- Reset the counter
-         v.slowCnt     := 0;
-         -- Reset the flag
-         v.slowSyncClk := not (r.slowSyncClk);  -- 833 kHz = 40MHz/(2 x 24)
-      else
-         -- Increment the counter
-         v.slowCnt := r.slowCnt + 1;
-      end if;
+      -- -- Check for last count
+      -- if r.slowCnt = 23 then
+         -- -- Reset the counter
+         -- v.slowCnt     := 0;
+         -- -- Reset the flag
+         -- v.slowSyncClk := not (r.slowSyncClk);  -- 833 kHz = 40MHz/(2 x 24)
+      -- else
+         -- -- Increment the counter
+         -- v.slowCnt := r.slowCnt + 1;
+      -- end if;
 
-      -- Register the variable for next clock cycle
-      rin <= v;
+      -- -- Register the variable for next clock cycle
+      -- rin <= v;
 
-      -- Outputs
-      pwrSyncFclk <= r.fastSyncClk;
-      pwrSyncSclk <= r.slowSyncClk;
+      -- -- Outputs
+      -- pwrSyncFclk <= r.fastSyncClk;
+      -- pwrSyncSclk <= r.slowSyncClk;
 
-   end process comb;
+   -- end process comb;
 
-   seq : process (clock160MHz, reset160MHz) is
-   begin
-      -- Asynchronous reset
-      if (reset160MHz = '1') then
-         r <= REG_INIT_C after TPD_G;
-      -- Clock 
-      elsif rising_edge(clock160MHz) then
-         -- Clock Enable
-         if (strobe40MHz = '1') then
-            r <= rin after TPD_G;
-         end if;
-      end if;
-   end process seq;
+   -- seq : process (clock160MHz, reset160MHz) is
+   -- begin
+      -- -- Asynchronous reset
+      -- if (reset160MHz = '1') then
+         -- r <= REG_INIT_C after TPD_G;
+      -- -- Clock 
+      -- elsif rising_edge(clock160MHz) then
+         -- -- Clock Enable
+         -- if (strobe40MHz = '1') then
+            -- r <= rin after TPD_G;
+         -- end if;
+      -- end if;
+   -- end process seq;
+
+   ---------------------------------
+   -- Not Synchronizing DC/DC to PLL
+   ---------------------------------
+   pwrSyncFclk <= '0';
+   pwrSyncSclk <= '0';
 
    ---------------------
    -- OSC Always enabled
