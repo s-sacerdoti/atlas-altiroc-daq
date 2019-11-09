e204de66d88bf149092d906c6d4eaa7ba52f7fdc
Sabrina Sacerdoti <sabrina.sacerdoti@cern.ch>
2019-11-09 21:29:43 +0100

    aliases for shift

---
+++ software/shift_9-11-2019.sh
@@ -0,0 +1,6 @@
+alias batch_800='python scripts/DevGui.py --ip $fpga_ab $fpga_ad config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC360.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_801='python scripts/DevGui.py --ip $fpga_ab $fpga_ad config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC400.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_802='python scripts/DevGui.py --ip $fpga_ab $fpga_ad config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC440.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_803='python scripts/DevGui.py --ip $fpga_ab $fpga_ad config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_800='python scripts/DevGui.py --ip $fpga_ab $fpga_ad config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC340.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_800='python scripts/DevGui.py --ip $fpga_ab $fpga_ad config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC320.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
