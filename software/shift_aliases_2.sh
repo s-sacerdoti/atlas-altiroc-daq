752e651fd4677bb3fbee95084e39c79ec547afa8
Sabrina Sacerdoti <sabrina.sacerdoti@cern.ch>
2019-11-11 02:18:51 +0100

    adding aliases and configs for night shift

---
+++ software/shift_aliases_2.sh
@@ -0,0 +1,4 @@
+alias batch_1100='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch10DAC430_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_1101='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch10DAC450_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_1200='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch13DAC430_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_1201='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch13DAC450_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
