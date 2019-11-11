54116b938288d23609520939672d2202c83fc88a
Sabrina Sacerdoti <sabrina.sacerdoti@cern.ch>
2019-11-11 18:15:33 +0100

    scan of B13 ch, and B13 alone

--- software/shift_aliases.sh
+++ software/shift_aliases.sh
@@ -5,3 +5,4 @@ alias batch_803='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml conf
 alias batch_804='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC380.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
 alias batch_805='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B3_master_ch7DAC440_scope.yml config/asic_config_B13_slave_ch7DAC400.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
 alias batch_806='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch7DAC400_scope.yml config/asic_config_B3_slave_ch7DAC440.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+alias batch_807='python scripts/DevGui.py --ip $fpga_ab --userYaml config/asic_config_B13_master_ch7DAC400_scope.yml --liveDisplay True'
--- software/shift_aliases_2.sh
+++ software/shift_aliases_2.sh
@@ -2,3 +2,5 @@ alias batch_1100='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml con
 alias batch_1101='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch10DAC450_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
 alias batch_1200='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch13DAC430_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
 alias batch_1201='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_ch13DAC450_scope.yml config/asic_config_B18_slave_ch5DAC420.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
+
+alias ch_dac='python scripts/DevGui.py --ip $fpga_ab $fpga_ad --userYaml config/asic_config_B13_master_oneCH.yml config/asic_config_B18_slave_allON.yml --liveDisplay True --refClkSel ExtSmaClk ExtSmaClk'
